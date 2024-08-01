c482c18d73bc573df482fd1321290bbe57d084aa
Grzegorz Burdek <grzegorz004-1993@o2.pl>
2024-08-01 16:46:02 +0200

    rm FileManager

--- FileManager/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-
-        FileManager fileManager = new FileManager();
-        fileManager.sortFilesInDirectory("C:\\Users\\IceCube\\Desktop\\Portfolio\\Java");
-    }
-}
